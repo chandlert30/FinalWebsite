581eafa0b4fcb2c3c28dde118ad8f40ddd4db867
chandlert30 <82465999+chandlert30@users.noreply.github.com>
2021-05-03 21:34:59 -0400

    submitting readyf for peer review

---
+++ FinalJS.js
@@ -0,0 +1,8 @@
+
+myitem.addEventListener("mouseover",onMouseOver);
+myitem = document.getElementById("firsttest");
+
+function onMouseOver() {
+    myitem.style.color = "red";
+    myitem.style.fontSize = "1.5em";
+}
